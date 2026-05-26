fb81808d98002d4a819f822439caf88fb5d14d54
HaniEdelbi <haniedelbi9@gmail.com>
2026-05-26 16:15:10 +0300

    Models Config

--- DrAppoitmentApi/Models/User.cs
+++ DrAppoitmentApi/Models/User.cs
@@ -1,7 +1,24 @@
-﻿namespace DrAppoitmentApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrAppoitmentApi.Models
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required,MaxLength(25)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, MaxLength(100)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        public string Role { get; set; } = string.Empty;
+
 
     }
 }
