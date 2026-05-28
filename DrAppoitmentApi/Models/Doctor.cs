6516460777656c0dbadefcd1ab38e186bc76f35e
HaniEdelbi <haniedelbi9@gmail.com>
2026-05-28 11:12:00 +0300

    Doctors model created

---
+++ DrAppoitmentApi/Models/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrAppoitmentApi.Models
+{
+    public class Doctor
+    {
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
+        public string Specialty { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
+        public string Region { get; set; } = string.Empty;
+
+    }
+}
