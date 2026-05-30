ad690f2cd06fdf6919ef33d9a2d3582a996b3b15
HaniEdelbi <haniedelbi9@gmail.com>
2026-05-30 13:26:17 +0300

    Patient Model created

---
+++ DrAppoitmentApi/Models/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrAppoitmentApi.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int DoctorId { get; set; }
+
+        [Required]
+        public string PatientName { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public string Status { get; set; } = "Pending";
+    }
+}
