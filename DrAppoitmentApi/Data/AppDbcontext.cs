7d4ccac214c2ac435a996d7fca5f852056fb43b6
HaniEdelbi <haniedelbi9@gmail.com>
2026-05-30 13:30:03 +0300

    Added Patient.cs to AppDbcontext and created the migration and updated the database

--- DrAppoitmentApi/Data/AppDbcontext.cs
+++ DrAppoitmentApi/Data/AppDbcontext.cs
@@ -11,5 +11,7 @@ namespace DrAppoitmentApi.Data
         }
         public DbSet<User> Users { get; set; }
         public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+
     }
 }
