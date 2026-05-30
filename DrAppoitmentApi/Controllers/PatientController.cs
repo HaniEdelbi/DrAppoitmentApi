cf5e396e3f7a4b26567d043c1b15981523945757
HaniEdelbi <haniedelbi9@gmail.com>
2026-05-30 13:33:02 +0300

    Patient CRUD operations created

---
+++ DrAppoitmentApi/Controllers/PatientController.cs
@@ -0,0 +1,74 @@
+﻿using DrAppoitmentApi.Data;
+using DrAppoitmentApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrAppoitmentApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private readonly AppDbcontext _context;
+
+        public PatientController(AppDbcontext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAppointments()
+        {
+            var appointments = await _context.Patients.ToListAsync();
+            if (appointments == null || appointments.Count == 0)
+                return NoContent();
+
+            return Ok(appointments);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAppointmentById(int id)
+        {
+            var appointment = await _context.Patients.FindAsync(id);
+            if (appointment == null)
+                return NotFound("Appointment not found");
+
+            return Ok(appointment);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BookAppointment(Patient patient)
+        {
+            _context.Patients.Add(patient);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            return Ok(patient);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> CancelAppointment(int id)
+        {
+            var appointment = await _context.Patients.FindAsync(id);
+            if (appointment == null)
+                return NotFound("Appointment not found");
+
+            _context.Patients.Remove(appointment);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            return Ok();
+        }
+    }
+}
